679fe4ece1abbc64b1a3a0dee81e470d598eedaf
maael <mae.me.uk@gmail.com>
2015-01-06 14:29:13 +0000

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     mocha = require('gulp-mocha');
 
-gulp.task('default', function () {
+gulp.task('test', function () {
   return gulp.src('test.js', {read: false})
         .pipe(mocha({reporter: 'dot'}));
 });
